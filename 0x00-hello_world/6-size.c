048073ed4cf457a5df13d7de996ff324b90c2e9b
Benzyma <ebenezamadu@gmail.com>
2022-12-12 05:47:57 -0800

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,11 +3,11 @@
  *main-print sizes
  *
  *
- *return:return 0
+ *Return:return 0
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s) \n", sizeof(char));
+printf("Size of a char: %D Byte(S)\n", sizeof(char));
 printf("Size of an int: %d bytes(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
