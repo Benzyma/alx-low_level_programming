7980c04ab404a5d8bfa706e1249792bb6a28381b
Benzyma <ebenezamadu@gmail.com>
2022-12-12 16:00:31 -0800

    if statement

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ *main- if sttatement
+ *
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+if (n > 0)
+{
+printf(" %d is positve\n", n);
+}
+else
+{
+printf(" %d is negative\n", n);
+}
+if (n == 0)
+{
+printf(" %d is zero\n", n);
+return (0);
+}
