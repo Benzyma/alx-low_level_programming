14e88ffe48b3756f0839860a8765ff247465594e
Benzyma <ebenezamadu@gmail.com>
2022-12-12 16:09:53 -0800

    done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,11 @@ if (n > 0)
 {
 printf(" %d is positve\n", n);
 }
-else
+else if (n < 0)
 {
 printf(" %d is negative\n", n);
 }
-if (n == 0)
+else if (n == 0)
 {
 printf(" %d is zero\n", n);
 return (0);
