3d4179f0fd708a42d57b72865670560bd2214c8a
Benzyma <ebenezamadu@gmail.com>
2022-12-14 04:03:19 -0800

    done 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#inclcude "main.h"
+/**
+ *main- to print alphabets
+ *
+ *
+ *Return: return 0
+ */
+void print_alphabet(void)
+{
+char c;
+ for (c = 'a' ; c <= 'z' ; c++)
+{
+_putchar(c);
+_putchar("\n");
+}
+ return (0)
+}
