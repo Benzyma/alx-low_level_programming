747a0434f5f564946c6c6febf594b90c6327b058
Benzyma <ebenezamadu@gmail.com>
2022-12-14 02:25:30 -0800

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,14 @@
 */
 int main(void)
 {
-c = _putchar;
-int _putchar ("%c\n", c);
+putchar ('_');
+putchar ('p');
+putchar ('u');
+putchar ('t');
+putchar ('c');
+putchar ('h');
+putchar ('a');
+putchar ('r');
+putchar ('\n');
 return (0);
 }
